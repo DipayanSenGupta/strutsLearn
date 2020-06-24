67abe3799681e2da0e9fd1982f85f486b072e087
dip.himu <dip.himu@gmail.com>
2020-06-24 13:54:48 +0600

    Add residue

--- src/main/action/HelloWorldAction.java
+++ src/main/action/HelloWorldAction.java
@@ -6,13 +6,19 @@ import main.java.MessageStore;
 public class HelloWorldAction extends ActionSupport {
 
 	private MessageStore messageStore;
+	private static int helloCount = 0;
 	
 	public String execute() {
 		messageStore = new MessageStore();
+		helloCount++;
 		return "success";
 	}
 	
 	public MessageStore getMessageStore() {
 		return messageStore;
+	}
+	
+	public int getHelloCount() {
+		return helloCount;
 	}
 }
