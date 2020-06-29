32733a7c14ffd33f4698a966d2997e51be1e1d69
dip.himu <dip.himu@gmail.com>
2020-06-29 12:34:49 +0600

    Add person bean

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,11 +1,48 @@
 package main.java;
 
+import java.util.Arrays;
+
 public class Person {
 
 	private String firstName;
 	private String lastName;
 	private String email;
 	private int age;
+	private String sport;
+	private String gender;
+	private String residency;
+	private boolean over21;
+	private String [] carModels;
+	public String[] getCarModels() {
+		return carModels;
+	}
+	public void setCarModels(String[] carModels) {
+		this.carModels = carModels;
+	}
+	public String getSport() {
+		return sport;
+	}
+	public void setSport(String sport) {
+		this.sport = sport;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getResidency() {
+		return residency;
+	}
+	public void setResidency(String residency) {
+		this.residency = residency;
+	}
+	public boolean isOver21() {
+		return over21;
+	}
+	public void setOver21(boolean over21) {
+		this.over21 = over21;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
@@ -31,9 +68,15 @@ public class Person {
 		this.age = age;
 	}
 	
-	public String toString() {
-		  return "First Name: " + getFirstName() + " Last Name:  " + getLastName() + 
-			        " Email:      " + getEmail() + " Age:      " + getAge() ;
-	}
+	public String toString()  {
+        return "First Name: " + getFirstName() + " | " +
+        " Last Name:  " + getLastName() + " | " +
+        " Favorite Sport: " + getSport() + " | " +
+        " Gender: " + getGender() + " | " +  
+        " Residency: " + getResidency() + " | " +
+        " Over 21: " + isOver21()  + " | " +
+        " Car models: " + Arrays.asList( getCarModels() ) ;
+        
+    }
 	
 }
